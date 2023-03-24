38be7bbb11c62c07d5b6117e45274ccf99a67a61
Joshua Monney <monneyjoshua14@gmail.com>
2023-03-23 21:10:27 -0700

    a function that multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+/**
+*mul - multiplies arguments
+*@a: argument 1 to be tested
+*@b: argument 2 to be tested
+*Description: multiples argument a and b
+*Return: multiply
+*/
+
+int mul(int a, int b)
+{
+	int multiply;
+
+	multiply = a * b;
+
+	return (multiply);
+}
